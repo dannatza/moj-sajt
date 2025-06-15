9c16fc6fdc7f277419d985cb30930bc2dede7890
dannatza <danilovukovic@tehnickacacak.edu.rs>
2025-06-15 16:13:41 +0200

    Add files via upload

--- moj-sajt-main/sajt/uspeh.js
+++ moj-sajt-main/sajt/uspeh.js
@@ -1,14 +1,14 @@
-window.filterByYear = function () {
+function filterByYear() {
   const startYear = parseInt(document.getElementById("startYear").value);
   const endYear = parseInt(document.getElementById("endYear").value);
   const items = document.querySelectorAll('#timeline .timeline-item');
 
   items.forEach(item => {
     const year = parseInt(item.getAttribute('data-year'));
-    if (year >= startYear && year <= endYear) {
-      item.style.display = 'block';
+    if (!isNaN(startYear) && !isNaN(endYear)) {
+      item.style.display = (year >= startYear && year <= endYear) ? 'block' : 'none';
     } else {
-      item.style.display = 'none';
+      item.style.display = 'block';
     }
   });
-};
+}
